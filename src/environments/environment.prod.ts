1337450d42cc92cbd61c477b621a2543c7e22ae8
Dima Parkhomenko <80668534+dumko23@users.noreply.github.com>
2021-10-12 10:23:38 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: /*'k_gl1dr4pi'*//*'k_fm3lowi1'*/'k_hclfpd19',
+  apiUrl250: 'Top250Movies',
+  apiUrl200: 'MostPopularMovies',
+  apiLinks: `https://imdb-api.com/en/API/ExternalSites`,
+  apiMovie: `https://imdb-api.com/en/API/Title`,
+  apiSearch: 'https://imdb-api.com/en/API/SearchMovie/'
 };
